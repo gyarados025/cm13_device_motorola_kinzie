b1aaa5997b51823a9c62774dee85d3f29340a864
Kshitij Kumar <40200770+gyarados025@users.noreply.github.com>
2018-07-15 00:27:30 +0530

    Update and rename cm.mk to lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -4,7 +4,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/kinzie/device.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
@@ -13,8 +13,8 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 DEVICE_PACKAGE_OVERLAYS += device/motorola/kinzie/overlay
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_kinzie
-PRODUCT_BRAND := Android
+PRODUCT_NAME := lineage_kinzie
+PRODUCT_BRAND := Motorola
 PRODUCT_DEVICE := kinzie
 PRODUCT_MODEL := XT1581
 PRODUCT_MANUFACTURER := motorola
