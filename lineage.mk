f9006ee318df1ccb88699001539368e8f5ac5cc5
Kshitij Kumar <40200770+gyarados025@users.noreply.github.com>
2018-07-15 13:12:31 +0530

    update Fingerprint and Description from 6.0.1

--- lineage.mk
+++ lineage.mk
@@ -14,8 +14,12 @@ DEVICE_PACKAGE_OVERLAYS += device/motorola/kinzie/overlay
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_kinzie
-PRODUCT_BRAND := Motorola
+PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := kinzie
 PRODUCT_MODEL := XT1581
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+BUILD_FINGERPRINT=motorola/kinzie_reteu/kinzie:6.0.1/MPKS24.107-54-6/7:user/release-keys \
+PRIVATE_BUILD_DESC="kinzie_reteu-user 6.0.1 MPKS24.107-54-6 7 release-keys"
